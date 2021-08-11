b5d00305cd229166f7fc0e1c2087fab24edf98fe
worldwidepark <83998627+worldwidepark@users.noreply.github.com>
2021-08-12 02:30:11 +0900

    index_and_process_create
    add and modified index.php and process_create.php

--- index.php
+++ index.php
@@ -1,44 +1,37 @@
-<?php
-$conn = mysqli_connect('localhost','root','12345','parks');
-
-$sql = 'SELECT * FROM topic';
-$result = mysqli_query($conn, $sql);
-$list="";
-while ($row = mysqli_fetch_array($result)){
-    $escaped_title = htmlspecialchars($row['title']);
-    $list= $list."<li><a
-    href=\"index.php?id={$row['id']}\">
-    {$escaped_title}</a></li>";
-};
-$article = array(
-    "title"=>"Welcome",
-    "description"=>"HELLO, WEB" );
-$update_link = '';
-$delete_link = '';
-$author="";
-
-if(isset($_GET['id'])){$filtered_id = mysqli_real_escape_string($conn,$_GET['id']);
-$sql = "SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE topic.id={$filtered_id}";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result);
-$article['title'] = htmlspecialchars($row['title']);
-$article['description'] =htmlspecialchars($row['description']);
-$article['name'] =htmlspecialchars($row['name']);
-$update_link = '<a href="update.php?id='.$_GET["id"].'">Update</a>';
-$delete_link ='
-<form action="process_delete.php" method = "post">
- <input type="hidden" name = "id" value= "'.$_GET["id"].'">
-<input type="submit" value = "delete">
-</form>
-';
-$author = "<p>by {$article['name']}</p>";
-
-print_r($article);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
+<table border='1'>
+ <tr>
+     <td>link</td><td>descriptin</td><td>削除</td>
+<?php
+$conn = mysqli_connect('localhost','root','12345','test');
+
+$sql = 'SELECT * FROM counter_table';
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result)){
+    $filtered = array(
+        'link'=>htmlspecialchars($row['link']),
+        'description'=>htmlspecialchars($row['description'])
+    );
+    ?>
+　　　　　　<tr>
+             <td><?=$filtered['link']?></td>
+             <td><?=$filtered['description']?></td>
+            　<td>
+                <form action="process_delete_author.php" method="post" 
+                onsubmit="if(!confirm('sure?')){return false;}">
+                    <input type = "hidden" name="id" value="<?=$filtered['id']?>">
+                    <input type="submit" value ="delete">
+       　　　　 </form>
+        　　　</td>
+            </tr>
+            <?php
+     }
+
+     ?>
+
+
+
 <head>
     <meta charset="UTF-8"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -53,18 +46,5 @@ print_r($article);
 <p><input type ='submit' value = ""></p>
 </form>
 
-
-
-<ol>
-<?php
-echo $list;
-?>
-</ol>
-<a href='create.php'>Create</a>
-<?=$update_link?>
- <?=$delete_link?>
-<h2><?=$article['title']?></h2>
-<?=$article['description']?>
-<?=$author?>
 </body>
 </html>
---
+++ process_create.php
@@ -0,0 +1,34 @@
+<?php
+$conn = mysqli_connect('localhost','root','12345','test');
+
+$filtered = array(
+    'link'=>mysqli_real_escape_string($conn,$_POST['link']),
+    'description'=>mysqli_real_escape_string($conn,$_POST['description'])
+);
+
+$sql="
+INSERT INTO counter_table
+(link, description, count )
+ VALUES(
+     '{$filtered['link']}',
+     '{$filtered['description']}',
+     0
+     )
+";
+
+$result = mysqli_query($conn,$sql);
+$sql="SELECT * FROM counter_table ORDER BY id DESC limit 1";
+$id_result= mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($id_result);
+
+
+
+if($id_result === false){
+    echo "保存失敗です。朴まで問い合わせてください。.";
+    error_log(mysqli_error($conn));
+} else{
+    echo "link.php?id=".$row["ID"]."をSLACKに入力してください。";
+}
+
+
+?>
